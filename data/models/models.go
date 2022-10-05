6de731b0d3830dfd83d08aae6e2aed30cd9108b0
Mark Astin <redwolf89@gmail.com>
2022-10-04 20:46:17 -0400

    finished lesson 38: Models

---
+++ data/models/models.go
@@ -0,0 +1,34 @@
+package models
+
+import "time"
+
+type Movie struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	Year int `json:"year"`
+	ReleaseDate time.Time `json:"release_date"`
+	Runtime int `json:"runtime"`
+	Rating int `json:"rating"`
+	MPAARating string `json:"mpaa_rating"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"update_at"`
+	MovieGenre []MovieGenre `json:"-"`
+}
+
+type Genre struct {
+	ID int `json:"id"`
+	GenreName string `json:"genre_name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"update_at"`
+
+}
+
+type MovieGenre struct {
+	ID int `json:"id"`
+	MovieID int `json:"movie_id"`
+	GenreID int `json:"genere_id"`
+	Genre Genre `json:"genere"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"update_at"`
+}
