fcf9e54ac33ecccb68c408591afe25169e9dd76a
Mark Astin <redwolf89@gmail.com>
2022-10-15 14:42:17 -0400

    finished lesson 56: Displaying movies by Genre

--- data/go-movies/src/App.js
+++ data/go-movies/src/App.js
@@ -4,62 +4,61 @@ import Movies from './components/Movies';
 import Admin from './components/Admin';
 import Home from './components/Home';
 import OneMovie from './components/OneMovie';
-import Genres from './components/Genres'
+import Genres from './components/Genres';
+import OneGenre from './components/OneGenre';
 
 export default function App() {
   return (
     <Router>
-    <div className="container">
-
-      <div className="row">
-        <h1 className="mt-3">
-          Go Watch a Movie!
-        </h1>
-        <hr className="mb-3"></hr>
-      </div>
-
-      <div className="row">
-        <div className="col-md-2">
-          <nav>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/movies">Movies</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/genres">Genres</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/admin">Manage Catalogue</Link>
-              </li>
-            </ul>
-          </nav>
+      <div className='container'>
+        <div className='row'>
+          <h1 className='mt-3'>Go Watch a Movie!</h1>
+          <hr className='mb-3'></hr>
         </div>
 
-        <div className="col-md-10">
-          <Switch>
-            
-            <Route path="/movies/:id" component={OneMovie} />
+        <div className='row'>
+          <div className='col-md-2'>
+            <nav>
+              <ul className='list-group'>
+                <li className='list-group-item'>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to='/movies'>Movies</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to='/genres'>Genres</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to='/admin'>Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
 
-            <Route path="/movies">
-              <Movies />
-            </Route>
-            <Route exact path="/genres">
-              <Genres />
-            </Route>
+          <div className='col-md-10'>
+            <Switch>
+              <Route path='/movies/:id' component={OneMovie} />
 
-            <Route path="/admin">
-              <Admin />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
+              <Route path='/movies'>
+                <Movies />
+              </Route>
+              <Route path='/genre/:id' component={OneGenre} />
+
+              <Route exact path='/genres'>
+                <Genres />
+              </Route>
+
+              <Route path='/admin'>
+                <Admin />
+              </Route>
+              <Route path='/'>
+                <Home />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
     </Router>
   );
 }
